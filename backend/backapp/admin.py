3fddd66740a0a83eaefae050a7531341e8b59ebd
toyman640 <falakoadebayor@gmail.com>
2024-05-27 12:42:41 +0100

    Add models and register to admin

--- backend/backapp/admin.py
+++ backend/backapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserStatus
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Status', {'fields': ('user_status',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'user_status')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserStatus)
--- backend/backapp/models.py
+++ backend/backapp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class UserSatus(models.Model):
+  status_name = models.CharField(max_length=100, verbose_name='User Satus', unique=True)
+  description = models.TextField(max_length=500, verbose_name='User Satus Description', blank=True)
+
+  def __str__(self):
+    return self.status_name
+
+class CustomUser(AbstractUser):
+  first_name = models.CharField(max_length=200, verbose_name='Firstname')
+  last_name = models.CharField(max_length=200, verbose_name='Lastname')
+  username = models.
+  email_address = models.
+  phone_number = models.
+  password = models.
+  user_status = models.ForeignKey(UserStatus, on_delete=models.SET_NULL, null=True, blank=True)
+
+  def __str__(self):
+      return self.username
