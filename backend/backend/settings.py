507804a86aa3eedf942c3649baa989df630ffb07
toyman640 <falakoadebayor@gmail.com>
2024-05-28 11:33:46 +0100

    Change Host email to company email

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,12 +92,12 @@ DATABASES = {
 }
 
 # Email Configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'falakoadebayor@gmail.com'
-EMAIL_HOST_PASSWORD = 'vgowurmobsxzqcry'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'buidlai.pro'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True  
+EMAIL_HOST_USER = 'info@buidlai.pro'
+EMAIL_HOST_PASSWORD = 'buidfceddaniel'
 
 
 # Password validation
